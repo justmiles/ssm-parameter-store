e2b6801c74209b224975a69a1837ef0daedea5cb
Miles Maddox <miles.maddox@edointeractive.com>
2018-11-13 13:25:14 -0600

    require confirmation before pushing changes

--- cmd/push.go
+++ cmd/push.go
@@ -5,10 +5,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var ()
+var (
+	noInput bool
+)
 
 func init() {
 	rootCmd.AddCommand(pushCmd)
+	pushCmd.PersistentFlags().BoolVar(&noInput, "no-input", false, "not not prompt for input")
+
 }
 
 // process the list command
@@ -16,6 +20,6 @@ var pushCmd = &cobra.Command{
 	Use:   "push",
 	Short: "push SSM Parameters",
 	Run: func(cmd *cobra.Command, args []string) {
-		ssmParameterStore.CMDPush(paths, format, directory)
+		ssmParameterStore.CMDPush(paths, format, directory, noInput)
 	},
 }
--- lib/commands_test.go
+++ lib/commands_test.go
@@ -6,6 +6,6 @@ import (
 
 func TestRun(t *testing.T) {
 	CMDPull([]string{"/ops", "/ops"}, "yaml", "/home/justmiles/go/src/github.com/justmiles/ssm-parameter-store/scratch")
-	CMDPush([]string{"/ops"}, "yaml", "/home/justmiles/go/src/github.com/justmiles/ssm-parameter-store/scratch")
+	CMDPush([]string{"/ops"}, "yaml", "/home/justmiles/go/src/github.com/justmiles/ssm-parameter-store/scratch", true)
 	CMDDiff([]string{"/ops"}, "yaml", "/home/justmiles/go/src/github.com/justmiles/ssm-parameter-store/scratch")
 }
